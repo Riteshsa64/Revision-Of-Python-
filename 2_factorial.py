2ded93c8c5ff7e3affcb9d69e5fccb283e18ff00
Riteshsa64 <saroderitesh807@gmail.com>
2026-04-16 23:05:39 +0530

    Add files via upload

---
+++ 2_factorial.py
@@ -0,0 +1,14 @@
+n=int(input("Enter a mumber")) 
+n = int(input("Enter a number: "))
+fact = 1
+
+if n == 0 or n == 1:
+    fact = 1
+else:
+    for i in range(1, n + 1):
+        fact = fact * i
+
+print("Factorial is:", fact)
+
+
+
